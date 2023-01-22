c9342527089c3f321fe3253a596f28fe9a5afcd9
Caio de Assis Guimaraes <caiodeassisguimaraes@hotmail.com>
2023-01-22 11:35:15 -0300

    first commit

---
+++ src/main/java/com/devsuperior/newdscomerce/NewdscomerceApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.newdscomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewdscomerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewdscomerceApplication.class, args);
+	}
+
+}
