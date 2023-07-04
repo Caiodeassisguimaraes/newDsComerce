226119765cdc1d374690efc63d27529b38083f7f
Caio de Assis Guimarães <caiodeassisguimaraes@hotmail.com>
2023-07-04 07:36:03 -0300

    OAuth2 JWT checklist

--- src/main/java/com/devsuperior/newdscomerce/config/ResourceServerConfig.java
+++ src/main/java/com/devsuperior/newdscomerce/config/ResourceServerConfig.java
@@ -6,6 +6,8 @@ import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import java.util.Arrays;
 
@@ -18,6 +20,16 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Autowired
     private Environment env;
+
+    @Autowired
+    private JwtTokenStore tokenStore;
+
+    private static final String [] PUBLIC = {"/oauth/token", "/h2-console/**"};
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore);
+    }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
