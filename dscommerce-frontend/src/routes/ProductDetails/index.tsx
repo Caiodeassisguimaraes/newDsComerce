547dc287e958c441e8eb7d8ce94b1633286a51f5
Caio de Assis Guimarães <caiodeassisguimaraes@hotmail.com>
2024-02-09 21:24:26 -0300

    pagina de catalogo

--- dscommerce-frontend/src/routes/ProductDetails/index.tsx
+++ dscommerce-frontend/src/routes/ProductDetails/index.tsx
@@ -1,7 +1,7 @@
 import './styles.css'
 import ButtonInverse from '../../components/ButtonInverse';
 import ButtonPrimary from '../../components/ButtonPrimary';
-import HeaderClient from '../../components/HeaderClient'
+import HeaderClient from '../../components/HeaderClient';
 import ProductDetailsCard from '../../components/ProductDetailsCard';
 
 export default function ProductDetails(){
