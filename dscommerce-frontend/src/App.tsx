c21fa85176019d4efb758e78bc855a7f4cd2a65b
Caio de Assis Guimarães <caiodeassisguimaraes@hotmail.com>
2024-02-11 11:03:27 -0300

    Redirecionando em caso de roda não encontrada. Not found route

--- dscommerce-frontend/src/App.tsx
+++ dscommerce-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Catalog from './routes/ClientHome/Catalog';
 import ProductDetails from './routes/ClientHome/ProductDetails';
 import ClientHome from './routes/ClientHome';
@@ -8,11 +8,12 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path = "/" element={<ClientHome/>}>
-          <Route index element = {<Catalog/>}/>
-          <Route path = "catalog" element = {<Catalog/>}/>
-          <Route path = "product-details/:productId" element = {<ProductDetails/>}/>
+        <Route path="/" element={<ClientHome />}>
+          <Route index element={<Catalog />} />
+          <Route path="catalog" element={<Catalog />} />
+          <Route path="product-details/:productId" element={<ProductDetails />} />
         </Route>
+        <Route path="*" element={<Navigate to="/" />}/>
       </Routes>
     </BrowserRouter>
   );
