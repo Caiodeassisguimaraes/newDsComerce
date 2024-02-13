d435ad7305dfc6ff6dcf7faa2d87a52ab9608bca
Caio de Assis Guimarães <caiodeassisguimaraes@hotmail.com>
2024-02-13 20:42:22 -0300

    Requisições para catalogo r por id

--- dscommerce-frontend/src/routes/ClientHome/Catalog/index.tsx
+++ dscommerce-frontend/src/routes/ClientHome/Catalog/index.tsx
@@ -3,16 +3,27 @@ import SearchBar from '../../../components/SearchBar';
 import CatalogCard from '../../../components/CatalogCard';
 import ButtonNextPage from '../../../components/ButtonNextPage';
 import * as productService from '../../../services/product-service';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Catalog() {
-    return (
+    
+    const [products, setProducts] = useState<ProductDTO[]>([]);
 
+    useEffect(() => {
+        axios.get("http://localhost:8080/products?size=12").then(response => {
+        setProducts(response.data.content)
+        });
+    }, []);
+
+    return (
+        
         <main>
             <section id="catalog-section" className="dsc-container">
                 <SearchBar />
                 <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
                     {
-                        productService.findAll().map(product => <CatalogCard keu={product.id} product={product} />)
+                        products.map(product => <CatalogCard key={product.id} product={product} />)
                     }
                 </div>
                 <ButtonNextPage />
