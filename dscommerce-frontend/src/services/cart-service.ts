45e93163031a77350cb396e58ced907edd290151
Caio de Assis Guimarães <caiodeassisguimaraes@hotmail.com>
2024-02-18 22:46:51 -0300

    Increace/decrease cart items

--- dscommerce-frontend/src/services/cart-service.ts
+++ dscommerce-frontend/src/services/cart-service.ts
@@ -6,20 +6,41 @@ export function saveCart(cart: OrderDTO) {
     cartRepository.save(cart);
 }
 
-export function getCart(): OrderDTO{
+export function getCart(): OrderDTO {
     return cartRepository.get();
 }
 
-export function addProduct(product: ProductDTO){
+export function addProduct(product: ProductDTO) {
     const cart = cartRepository.get();
     const item = cart.items.find(item => item.productId === product.id);
-    if (!item){
+    if (!item) {
         const newItem = new OrderItemDTO(product.id, 1, product.name, product.price, product.imgUrl);
         cart.items.push(newItem);
         cartRepository.save(cart);
     }
 }
 
-export  function clearCart(){
+export function clearCart() {
     return cartRepository.clear();
+}
+
+export function increaseItem(productId: number) {
+    const cart = cartRepository.get();
+    const item = cart.items.find(x => x.productId === productId);
+    if (item) {
+        item.quantity++;
+        cartRepository.save(cart);
+    }
+}
+
+export function decreaseItem(productId: number) {
+    const cart = cartRepository.get();
+    const item = cart.items.find(x => x.productId === productId);
+    if (item) {
+        item.quantity--;
+        if (item.quantity < 1) {
+            cart.items = cart.items.filter(x => x.productId !== productId);
+        }
+        cartRepository.save(cart);
+    }
 }
