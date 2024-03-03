826edd6c6ab375374b1bfa2612ef84a0de969955
Caio de Assis Guimarães <caiodeassisguimaraes@hotmail.com>
2024-03-03 16:45:24 -0300

    isAuthenticated

--- dscommerce-frontend/src/services/auth-service.ts
+++ dscommerce-frontend/src/services/auth-service.ts
@@ -1,9 +1,10 @@
 import QueryString from "qs";
-import { CredentialsDTO } from "../models/auth";
+import { AccessTokenPayloadDTO, CredentialsDTO } from "../models/auth";
 import { CLIENT_ID, CLIENT_SECRET } from "../utils/system";
 import { AxiosRequestConfig } from "axios";
 import { requestBckend } from "../utils/requests";
 import * as accessTokenRepository from '../localstorage/access-token-repository';
+import jwtDecode from "jwt-decode";
 
 export function loginRequest(loginData: CredentialsDTO) {
     const headers = {
@@ -17,7 +18,7 @@ export function loginRequest(loginData: CredentialsDTO) {
         method: "POST",
         url: "/oauth/token",
         data: requestBody,
-        headers : headers
+        headers: headers
         //headers
     }
 
@@ -32,6 +33,26 @@ export function saveAccessToken(token: string) {
     accessTokenRepository.save(token);
 }
 
-export function getAccessToken(){
+export function getAccessToken() {
     return accessTokenRepository.get();
+}
+
+export function getAccessTokenPayload(): AccessTokenPayloadDTO | undefined {
+    try {
+        const token = accessTokenRepository.get();
+        return token == null ? undefined : (jwtDecode(token) as AccessTokenPayloadDTO);
+    } catch (error) {
+        return undefined;
+    }
+}
+
+export function isAuthenticated(): boolean {
+    let tokenPayload = getAccessTokenPayload();
+    if(tokenPayload && tokenPayload.exp * 1000 > Date.now()){
+        return true
+    } else{
+        return false
+    }
+
+    //return tokenPayload && tokenPayload.exp * 1000 > Date.now() ? true : false;
 }
